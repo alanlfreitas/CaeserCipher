f2a35b9883f7c4b02a05c88fb419bab619f825f5
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-09-28 16:45:08 +0000

    Two functions added for practice. Func to calculate the number of paint cans and func to check if a number is prime or not.

--- main.py
+++ main.py
@@ -17,8 +17,30 @@
 # greet_person("Bond")
 
 # Functions with more than 1 input
-# Changing order of arguments
-def greet_with(name, location):
-  print(f"Hello, {name}.")
-  print(f"What is it like in {location}?")
-greet_with(location = "Manaus", name = "Alan")
+# Changing order of arguments-
+# def greet_with(name, location):
+#   print(f"Hello, {name}.")
+#   print(f"What is it like in {location}?")
+# greet_with(location = "Manaus", name = "Alan")
+
+# import math
+# def paint_calc(height, width, cover):
+#     #math.ceil to round the result up
+#     cans = math.ceil((height * width) / coverage)
+#     print(f"You'll need {cans} cans of paint.")
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+def prime_checker(number):
+    prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            prime = False
+    if prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+n = int(input("Check this number: "))
+prime_checker(number=n)
