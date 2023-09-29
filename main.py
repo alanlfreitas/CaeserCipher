a15206c5a924385bd48e9ffedfc99e7b3dc2427b
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-09-29 15:49:26 +0000

    Encode function implemented

--- main.py
+++ main.py
@@ -33,14 +33,31 @@
 # coverage = 5
 # paint_calc(height=test_h, width=test_w, cover=coverage)
 
-def prime_checker(number):
-    prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            prime = False
-    if prime:
-        print("It's a prime number.")
-    else:
-        print("It's not a prime number.")
-n = int(input("Check this number: "))
-prime_checker(number=n)
+# def prime_checker(number):
+#     prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             prime = False
+#     if prime:
+#         print("It's a prime number.")
+#     else:
+#         print("It's not a prime number.")
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ''
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    cipher_text += new_letter
+  print(f"The encoded text is: {cipher_text}")
+
+encrypt(plain_text = text, shift_amount = shift)
