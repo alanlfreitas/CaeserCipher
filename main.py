6ab52893eeb9953fa99af58981aa1bc2ce043510
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-10-02 14:21:59 +0000

    feat: added a logo, added a while loop to ask user if it wants to continue. project finished!

--- main.py
+++ main.py
@@ -47,20 +47,36 @@
 
 alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caeser(start_text, shift_amount, cipher_direction):
   final_text = ""
   if cipher_direction == "decode":
-      shift_amount = shift_amount * -1
-  for letter in start_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    final_text += alphabet[new_position]
+      shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      final_text += alphabet[new_position]
+    else:
+      final_text += char
+  
   print(f"The {cipher_direction}d text is: {final_text}")
-caeser(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+from art import logo
+print(logo)
+
+want_to_continue = True
+while want_to_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+  caeser(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+  result = input("'Yes' if you want to continue, 'No' if you want to stop: ").lower()
+  if result == "no":
+    want_to_continue = False
+    print("Goodbye, hope you enjoyed.")
 
 # def encrypt(plain_text, shift_amount):
 #   cipher_text = ''
